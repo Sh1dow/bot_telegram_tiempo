3e59bee7c9caa9906f901f30d00f5761d20a2490
Sh1dow <davidmar1999@gmail.com>
2020-11-12 19:59:01 +0000

    corregida doble asignacion

--- Programa Factorizado/WeatherCls.py
+++ Programa Factorizado/WeatherCls.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 
-url = url = "https://api.openweathermap.org/data/2.5/onecall?lat=28.4853&lon=-16.32014&appid=c9b10045ffec1ed3b67f9e67daeb3700&units=metric"
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=28.4853&lon=-16.32014&appid=c9b10045ffec1ed3b67f9e67daeb3700&units=metric"
 
 class weather:
 
